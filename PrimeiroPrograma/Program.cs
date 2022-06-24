e22bb9b2e24c5ceb6397c7f4023d44b1b8e565f3
Alef Silva <alef.dev.silva@gmail.com>
2022-06-24 12:12:45 -0300

    Excluindo arquivos desnecessarios

--- PrimeiroPrograma/Program.cs
+++ PrimeiroPrograma/Program.cs
@@ -10,17 +10,6 @@ namespace primeiroPrograma
         {
 
 
-            //ESTRUTURAS DE CONTROLE
-
-            //double valorSaque = 10.0;
-            //Sacar();
-            //Depositar();
-            //Console.WriteLine("Digite a operacao que deseja executar: ");
-            //char operacao = char.Parse(Console.ReadLine());
-            //Calculadora(operacao);
-            //ESTRUTURAS DE REPETIÇÃO
-
-            //Investimento();
             Sacar();
 
             Cliente cliente = new Cliente();
