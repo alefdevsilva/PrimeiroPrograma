f785de67440041e28bb4f95866842802939e74bb
Alef Silva <alef.dev.silva@gmail.com>
2022-06-28 14:46:16 -0300

    Tirando os espaços vazios entre as chaves e excluindo comentários

--- PrimeiroPrograma/Program.cs
+++ PrimeiroPrograma/Program.cs
@@ -8,27 +8,6 @@ namespace primeiroPrograma
     {
         public static void Main(string[] args)
         {
-            /*Cliente cliente = new Cliente();
-            cliente.Nome = "Alef";
-            cliente.Cpf = "46565465450";
-            cliente.Rg = "658745874";
-            
-            //ContaPoupanca poupanca = new ContaPoupanca(cliente, 1234, 10000);
-            
-            
-            ContaCorrente corrente = new ContaCorrente(cliente, 1234, 10000);
-
-            Console.WriteLine("Poupanca: " + corrente.Titular.Nome);
-            Console.WriteLine("Digite o valor a ser sacado");
-            double valorSaque = double.Parse(Console.ReadLine());
-            corrente.Sacar(valorSaque);
-            corrente.consultaSaldo();
-            Console.WriteLine("Digite o valor a ser depositado: ");
-            double valorDeposito = double.Parse(Console.ReadLine());
-            corrente.Depositar(valorDeposito);
-            corrente.consultaSaldo();
-            */
-
             while (true)
             {
                 Cliente cliente;
@@ -87,7 +66,6 @@ namespace primeiroPrograma
                             {
                                 continue;
                             }
-                            
                         }
                         else if (escolha == 2)
                         {
@@ -105,8 +83,6 @@ namespace primeiroPrograma
                             Console.Clear();
                         }
                     }
-                      
-
                 }
                 else if (opcao == 2)
                 {
@@ -164,8 +140,6 @@ namespace primeiroPrograma
                             Console.Clear();
                         }
                     }
-
-
                 }
                 else
                 {
